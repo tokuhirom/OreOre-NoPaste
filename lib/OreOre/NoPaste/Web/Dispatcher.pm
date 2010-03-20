8e91fe966f120c4ee48ad050880e102b267e6d94
tokuhirom <tokuhirom@gmail.com>
2010-03-20 23:17:36 +0900

    use Amon::Web::Dispatcher::Lite

--- lib/OreOre/NoPaste/Web/Dispatcher.pm
+++ lib/OreOre/NoPaste/Web/Dispatcher.pm
@@ -1,25 +1,41 @@
 package OreOre::NoPaste::Web::Dispatcher;
-use Amon::Web::Dispatcher;
-use feature 'switch';
-use 5.010;
+use Amon::Web::Dispatcher::Lite -base;
+use Data::UUID;
+use Encode;
 
-sub dispatch {
-    my ($class, $c) = @_;
-    my $req = $c->request;
-    given ([$req->method, $req->path_info]) {
-        when (['GET', '/']) {
-            return call("Root", 'index');
-        }
-        when (['POST', '/post']) {
-            return call("Root", 'post');
-        }
-        when (['GET', qr{^/entry/(.+)$}]) {
-            return call("Root", 'show', $1);
-        }
-        default {
+my $uuid_gen = Data::UUID->new;
+
+get '/' => sub {
+    render("index.mt");
+};
+
+post '/post' => sub {
+    if (my $body = param_decoded("body")) {
+        my $uuid = $uuid_gen->create_str();
+        my $sth = db->prepare("INSERT INTO entry (id, body) values (?, ?)") or die db->errstr;
+        $sth->execute($uuid, $body) or die db->errstr;
+        return redirect("/entry/$uuid");
+    } else {
+        return redirect('/');
+    }
+};
+
+get '/entry/{entry_id}' => sub {
+    my ($c, $args) = @_;
+    my $id = $args->{entry_id};
+
+    if ($id) {
+        my $sth = db->prepare('SELECT body FROM entry WHERE id=?') or die db->errstr;
+        $sth->execute($id) or die db->errstr;
+        my ($body) = $sth->fetchrow_array();
+        if ($body) {
+            return render('show.mt', $body);
+        } else {
             return res_404();
         }
+    } else {
+        return res_404();
     }
-}
+};
 
 1;
