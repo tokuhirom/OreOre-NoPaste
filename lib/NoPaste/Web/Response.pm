6c6e94045af0ccb199df699da93601ae7ec844c4
tokuhirom <tokuhirom@gmail.com>
2010-08-03 03:38:07 +0900

    use abs path for Location header

--- lib/NoPaste/Web/Response.pm
+++ lib/NoPaste/Web/Response.pm
@@ -10,4 +10,20 @@ sub not_found {
     $self;
 }
 
+sub redirect {
+    my ( $self, $location ) = @_;
+    my $url = do {
+        if ( $location =~ m{^https?://} ) {
+            $location;
+        }
+        else {
+            my $url = NoPaste->context->request->base;
+            $url      =~ s!/+$!!;
+            $location =~ s!^/+([^/])!/$1!;
+            $url .= $location;
+        }
+    };
+    $self->SUPER::redirect($url);
+}
+
 1;
